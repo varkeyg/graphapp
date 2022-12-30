e2129933fac5b7b7ec388101178f4755c5e3f52f
Varkey, Geo <geo.varkey@gmail.com>
2022-12-30 13:58:17 -0800

    Intermediate Update

--- generate_vert_edges.sh
+++ generate_vert_edges.sh
@@ -24,4 +24,4 @@ select distinct holder_cik ||cusip || '-'|| period_date as '~id',
        date_filed
   from holdings;" > edges.csv
 
-  python3 csv-gremlin.py holder_vertices.csv > holder_vertices.gremlin
+  python3 /home/graph-user/amazon-neptune-tools/csv-gremlin/csv-gremlin.py holder_vertices.csv > holder_vertices.gremlin
