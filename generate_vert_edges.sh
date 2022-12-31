5fee854e7a86509d341650d3edf145401c0b1b94
Varkey, Geo <geo.varkey@gmail.com>
2022-12-30 22:33:14 -0800

    Intermediate Update

--- generate_vert_edges.sh
+++ generate_vert_edges.sh
@@ -10,7 +10,7 @@ select distinct cusip || '-'|| period_date  as '~id',
        sec_type,
        substr(period_date,1,4)||'-'||substr(period_date,5,2)||'-'||substr(period_date,7,2) as 'period_date:Date',
        sum(cast (market_value as decimal)) as 'market_value:Double',
-       sum(cast (quantity as integer)) as 'quantity:Int'
+       sum(cast (quantity as integer)) as 'quantity:Long'
   from holdings
   where sym in ('AAPL', 'CAT')
 group by sym, cusip, holding_name, period_date
